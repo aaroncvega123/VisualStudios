bed2a7688f505208e8167cbab68fda456b76ede2
Aaron Vega <aaroncvega123@gmail.com>
2018-03-18 15:04:31 -0700

    Initial commit

---
+++ C#/BowlingKata/BowlingKata/ScoreBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKata
+{
+    class ScoreBox
+    {
+        private int score;
+        private char status;
+    }
+}
---
+++ C#/BowlingKata/BowlingKata/ScoreCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKata
+{
+    class ScoreCard
+    {
+        ScoreBox[] scores;
+
+        public void UpdateScore(int score)
+        {
+
+        }
+    }
+}
